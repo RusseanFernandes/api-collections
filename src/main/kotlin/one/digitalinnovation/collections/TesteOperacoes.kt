e4d0db2c0640343c04d71a06585be049f592b9dd
RusseanFernandes <56703764+RusseanFernandes@users.noreply.github.com>
2021-03-28 23:43:31 -0300

    Operações

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -0,0 +1,30 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val salarios = doubleArrayOf(1010.0,2400.5,1009.5)
+
+    salarios.forEach { println(it) }
+    println("------------------")
+    salarios.sort()
+    for (valor in salarios){
+        println(valor)
+    }
+    println("------------------")
+    println("Maior salário: ${salarios.max()}")
+    println("Menor salário: ${salarios.min()}")
+    println("Média salário: ${salarios.average()}")
+
+    val salarioMarioQue1000 = salarios.filter { it>1200 }
+    println("------------------")
+    salarioMarioQue1000.forEach { println(it) }
+
+    println("------------------")
+    println(salarios.count { it in 1009.7..4000.0 })
+    println("------------------")
+    println(salarios.find { it == 500.0 })
+    println(salarios.find { it == 1009.5 })
+    println("------------------")
+    println(salarios.any { it == 1010.0 })
+    println(salarios.any { it == 5000.0 })
+
+}
